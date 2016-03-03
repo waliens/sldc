4b4e6b8f089ab4a7302466ca959e3dd0efba9849
Romain Mormont <r.mormont@student.ulg.ac.be>
2016-03-03 14:04:53 +0100

    add workflow test

---
+++ test/olive/__init__.py
@@ -0,0 +1,11 @@
+from test.olive.image import NumpyTileBuilder, NumpyTile, NumpyImage
+from test.olive.olive_workflow import OliveWorkflow, OliveDispatcherClassifier
+from test.olive.pizza_workflow import PizzaWorkflow, PizzaDispatcherClassifier, SmallPizzaClassifier, BigPizzaClassifier, \
+    ObjectType, BigPizzaRule, SmallPizzaRule, PizzaSegmenter
+from test.olive.workflow import PizzaPostProcessor, PizzaOliveLinker, PizzaImageProvider
+
+__all__ = ["OliveDispatcherClassifier", "OliveWorkflow", "NumpyImage",
+           "NumpyTile", "NumpyTileBuilder", "PizzaSegmenter", "SmallPizzaRule",
+           "BigPizzaRule", "ObjectType", "BigPizzaClassifier", "SmallPizzaClassifier",
+           "PizzaDispatcherClassifier", "PizzaWorkflow", "PizzaImageProvider",
+           "PizzaOliveLinker", "PizzaPostProcessor"]
