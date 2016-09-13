a8a00d1a94cda49801c9d70afbf4bebfca43bf40
Romain Mormont <romainmormont@hotmail.com>
2016-09-13 16:34:13 +0200

    make setup script working

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from distutils.core import setup
 
+"""
+Dependencies:
+* open-cv2
+* numpy
+* Shapely
+"""
 setup(
     name="sldc",
     version="1.0",
     description="SLDC, a generic workflow for object detection and classification in large images.",
     author="Romain Mormont",
     author_email="romain.mormont@gmail.com",
+    packages=["sldc"]
 )
