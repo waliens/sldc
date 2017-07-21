03e91987a030c5d0b6a256e05b3402a7bc83d02d
Romain Mormont <romainmormont@hotmail.com>
2017-07-21 18:52:58 +0200

    more tests for information

--- test/test_information.py
+++ test/test_information.py
@@ -5,7 +5,7 @@ import numpy as np
 from numpy.testing import assert_array_equal
 from shapely.geometry import Polygon, Point
 
-from sldc import WorkflowInformation, WorkflowTiming, merge_information
+from sldc import WorkflowInformation, WorkflowTiming, merge_information, ChainInformation
 
 
 class TestInformation(TestCase):
@@ -97,6 +97,31 @@ class TestInformation(TestCase):
             first._is_compatible(third)
 
 
-class TestChainInformation(object):
-    def testBase(self):
-        pass
+class TestChainInformation(TestCase):
+    def testChainInformation(self):
+        p1, p2 = Point(0, 0), Point(1, 0)
+        polygons = [p1, p2]
+        labels = [1, 2]
+        timing = WorkflowTiming()
+        w1 = WorkflowInformation(polygons=polygons, labels=labels, timing=timing)
+        w2 = WorkflowInformation(polygons=polygons, labels=labels, timing=timing, others=(labels, "other"))
+        workflows = [("w1", w1), ("w2", w2)]
+
+        chain_info = ChainInformation()
+        chain_info.append("w1", w1)
+        chain_info.append("w2", w2)
+
+        assert_array_equal(["w1", "w2"], chain_info.info_labels)
+
+        self.assertEqual(len(chain_info), 2)
+        self.assertEqual(chain_info["w1"], w1)
+        self.assertEqual(chain_info["w2"], w2)
+        self.assertEqual(chain_info.information("w1"), w1)
+        self.assertEqual(chain_info.information("w2"), w2)
+
+        for (e_label, e_workflow), (a_label, a_workflow) in zip(chain_info, workflows):
+            self.assertEqual(e_label, a_label)
+            self.assertEqual(e_workflow, a_workflow)
+
+        assert_array_equal(chain_info.labels, np.array(labels + labels))
+        assert_array_equal(chain_info.polygons, np.array(polygons + polygons, dtype=np.object))
